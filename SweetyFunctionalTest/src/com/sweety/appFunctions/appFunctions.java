70fbdab6acaafbd506574491a209d26e0d0bbcd8
rajeshpullur <rajesh.pullur@gmail.com>
2016-05-17 17:44:40 +0530

    improvements

--- SweetyFunctionalTest/src/com/sweety/appFunctions/appFunctions.java
+++ SweetyFunctionalTest/src/com/sweety/appFunctions/appFunctions.java
@@ -1,14 +1,22 @@
 package com.sweety.appFunctions;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
+import com.google.common.collect.MultimapBuilder.ListMultimapBuilder;
 import com.sweety.utility.UtilityLib;
 
 public class appFunctions {
@@ -16,80 +24,164 @@ public class appFunctions {
 	public WebDriver driver;
 	public WebDriverWait wait;
 	UtilityLib utilFuncs = new UtilityLib();
-	Properties readConfig = utilFuncs
-			.loadProperties("C:\\config.properties");
-	Properties readRepo = utilFuncs
-			.readRepository("C\\repository.properties");
+
+	Properties readLoginData = utilFuncs
+			.loadProperties("C:\\SweetyProject\\TestData\\loginData.properties");
+	Properties readLevelData = utilFuncs
+			.loadProperties("C:\\SweetyProject\\TestData\\levelEntryData.properties");
+	Properties readReportData = utilFuncs
+			.loadProperties("C:\\SweetyProject\\TestData\\ReportData.properties");
+
+	Properties readobjLogin = utilFuncs
+			.readRepository("C:\\SweetyProject\\Repository\\login.properties");
+
+	Properties readobjEntry = utilFuncs
+			.readRepository("C:\\SweetyProject\\Repository\\levelEntry.properties");
+
+	Properties readobjReport = utilFuncs
+			.readRepository("C:\\SweetyProject\\Repository\\Report.properties");
 
 	public void login() {
 
-		driver = new FirefoxDriver();
-		driver.get(readConfig.getProperty("appUrl"));
-		driver.manage().window().maximize();
-		wait = new WebDriverWait(driver, 20);
-		wait.until(
-				ExpectedConditions.presenceOfElementLocated(By.id(readRepo
-						.getProperty("userid")))).sendKeys(
-				readConfig.getProperty("userName"));
-		driver.findElement(By.id(readRepo.getProperty("password"))).sendKeys(
-				readConfig.getProperty("password"));
-		driver.findElement(By.xpath(readRepo.getProperty("loginButton")))
-				.click();
-		String loginMessage = driver.findElement(
-				By.xpath(readRepo.getProperty("loginMessage"))).getText();
-		loginMessage.equalsIgnoreCase(readConfig.getProperty("loginMessage"));
-	}
-
-	public void newLevelEntry() {
-
 		try {
-			driver.findElement(By.xpath(readRepo.getProperty("level"))).click();
+			driver = new FirefoxDriver();
+			driver.get(readLoginData.getProperty("appUrl"));
+			driver.manage().window().maximize();
+			wait = new WebDriverWait(driver, 20);
 			wait.until(
 					ExpectedConditions.presenceOfElementLocated(By
-							.xpath(readRepo.getProperty("addNew")))).click();
+							.id(readobjLogin.getProperty("userid")))).sendKeys(
+					readLoginData.getProperty("userName"));
+			driver.findElement(By.id(readobjLogin.getProperty("password")))
+					.sendKeys(readLoginData.getProperty("password"));
+			driver.findElement(
+					By.xpath(readobjLogin.getProperty("loginButton"))).click();
+			String loginMessage = driver.findElement(
+					By.xpath(readobjLogin.getProperty("loginMessage")))
+					.getText();
+			Assert.assertEquals(loginMessage,
+					readLoginData.getProperty("loginMessage"));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	public void newLevelEntry(String data) {
+
+		try {
+			driver.findElement(By.xpath(readobjEntry.getProperty("level")))
+					.click();
 			wait.until(
-					ExpectedConditions.presenceOfElementLocated(By.id(readRepo
-							.getProperty("entryLevel")))).sendKeys(
-					readConfig.getProperty("levelEntry"));
-			driver.findElement(By.xpath(readRepo.getProperty("submit")))
+					ExpectedConditions.presenceOfElementLocated(By
+							.xpath(readobjEntry.getProperty("addNew"))))
+					.click();
+			wait.until(
+					ExpectedConditions.presenceOfElementLocated(By
+							.id(readobjEntry.getProperty("entryLevel"))))
+					.sendKeys(data);
+
+			driver.findElement(By.xpath(readobjEntry.getProperty("submit")))
 					.click();
 			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 			String succMessage = driver.findElement(
-					By.xpath(readRepo.getProperty("submitMessage"))).getText();
+					By.xpath(readobjEntry.getProperty("submitMessage")))
+					.getText();
 			System.out.println("Success Message is : " + succMessage);
-			boolean message = succMessage.equalsIgnoreCase(readConfig
-					.getProperty("successMessage"));
-			System.out.println("Success Message matched with Expected : "
-					+ message);
+			Assert.assertEquals(succMessage,
+					readLevelData.getProperty("successMessage"));
+			
+			String entryValue = driver.findElement(By.xpath(readobjEntry.getProperty("entryValue"))).getText();
+			Assert.assertEquals(entryValue, data+" mg/dl");
+
 		} catch (Exception e) {
 			String errorMsg = driver.findElement(
-					By.xpath(readRepo.getProperty("errorMessage"))).getText();
-			boolean errMsg = errorMsg.equalsIgnoreCase(readConfig
-					.getProperty("errorMessage"));
+					By.xpath(readobjEntry.getProperty("errorMessage")))
+					.getText();
+			Assert.assertEquals(errorMsg,
+					readLevelData.getProperty("errorMessage"));
+
 			System.out.println("Error Message is : " + errorMsg);
-			System.out.println("Error Message matched with expected message : "
-					+ errMsg);
 
 		}
 	}
 
 	public void report() throws InterruptedException {
 
-		driver.findElement(By.xpath(readRepo.getProperty("report"))).click();
+		driver.findElement(By.xpath(readobjReport.getProperty("report")))
+				.click();
 		wait.until(
-				ExpectedConditions.presenceOfElementLocated(By.xpath(readRepo
-						.getProperty("monthlyReport")))).click();
+				ExpectedConditions.presenceOfElementLocated(By
+						.xpath(readobjReport.getProperty("monthlyReport"))))
+				.click();
 		Thread.sleep(5000);
 		String reportHeading = wait.until(
-				ExpectedConditions.presenceOfElementLocated(By.xpath(readRepo
-						.getProperty("reportName")))).getText();
-		reportHeading.equalsIgnoreCase(readConfig.getProperty("reportName"));
+				ExpectedConditions.presenceOfElementLocated(By
+						.xpath(readobjReport.getProperty("reportName"))))
+				.getText();
+		Assert.assertEquals(reportHeading,
+				readReportData.getProperty("reportName"));
+
 		String reportDate = driver.findElement(
-				By.xpath(readRepo.getProperty("reportDate"))).getText();
-		reportDate.equalsIgnoreCase(readConfig.getProperty("reportDate"));
+				By.xpath(readobjReport.getProperty("reportDate"))).getText();
+		Assert.assertEquals(reportDate,
+				readReportData.getProperty("reportDate"));
 
 		System.out.println("Report Name = " + reportHeading);
 		System.out.println("Report Date = " + reportDate);
+
+	}
+
+	public void verifyReport() {
+		List<Integer> allValues = new ArrayList<Integer>();
+		List<WebElement> allEntries = driver.findElements(By
+				.xpath(readobjReport.getProperty("allEntries")));
+
+		try {
+			for (int a = 0; a < allEntries.size(); a++) {
+				allValues.add(Integer.parseInt(allEntries.get(a).getText()));
+				System.out.println("verifyReport() : " + allValues.get(a));
+			}
+
+			Integer maxVal = Collections.max(allValues);
+			Integer minVal = Collections.min(allValues);
+
+			System.out.println("Max value is : " + maxVal);
+			System.out.println("Min Value is : " + minVal);
+
+			int sum = 0;
+			Iterator<Integer> iterator = allValues.iterator();
+			while (iterator.hasNext()) {
+				int val = iterator.next();
+				sum = sum + val;
+			}
+
+			// calculate average value
+			Integer average = sum / allValues.size();
+			
+			System.out
+					.println("verifyReport() : [ Rounded ] Average value of array elements is : "
+							+ Math.round(average));
+
+			Integer minValue = Integer.parseInt(driver.findElement(
+					By.xpath(readobjReport.getProperty("minValue"))).getText());
+			Integer maxValue = Integer.parseInt(driver.findElement(
+					By.xpath(readobjReport.getProperty("maxValue"))).getText());
+			Integer avgValue = Integer.parseInt(driver.findElement(
+					By.xpath(readobjReport.getProperty("avgValue"))).getText());
+			
+			Assert.assertEquals(minValue, minVal);
+			Assert.assertEquals(maxValue, maxVal);
+			Assert.assertEquals(avgValue, average);
+
+			System.out.println("Minimum value : " + minValue);
+			System.out.println("Maximum value : " + maxValue);
+			System.out.println("Average value : " + avgValue);
+		} catch (Exception e) {
+			System.out.println("verifyReport() : Exception Occured"
+					+ e.getMessage());
+			e.printStackTrace();
+		}
 
 	}
 
--- SweetyFunctionalTest/src/com/sweety/testCase/FunctionalTest.java
+++ SweetyFunctionalTest/src/com/sweety/testCase/FunctionalTest.java
@@ -1,9 +1,14 @@
 package com.sweety.testCase;
 
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
 import com.sweety.appFunctions.appFunctions;
 
 
@@ -12,23 +17,39 @@ public class FunctionalTest {
 	
 	appFunctions appFunc = new appFunctions();
 	
-	@BeforeMethod
+	
+	@BeforeClass
 	public void login(){
 		
 		appFunc.login();
 		
 	}
 
-	@Test
-	public void FuncTest() throws InterruptedException{
+	@Test(dataProvider="getData")
+	public void FuncTest(String lData) throws InterruptedException{
 				
-		appFunc.newLevelEntry();
+		appFunc.newLevelEntry(lData);
 		appFunc.report();
+		appFunc.verifyReport();
+		
+	}
+	
+	@DataProvider
+	public Object[][] getData(){
+		Object[][] data = new Object[5][1];
+		
+		data[0][0]="120";
+		data[1][0]="200";
+		data[2][0]="250";
+		data[3][0]="-300";
+		data[4][0]="123";
+		
+		return data;
 	}
 
-	@AfterMethod
+
+	@AfterClass
 	public void closeApp() {
-		
 		appFunc.closeApp();
 	}
 
